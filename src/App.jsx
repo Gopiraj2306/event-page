0f92ed1d8ce528cb8813ef2191c8f5cd57ab3ac4
gopiraj2306 <gopi.venkatesann@gmail.com>
2025-05-29 08:37:51 +0530

    design for map

--- src/App.jsx
+++ src/App.jsx
@@ -36,7 +36,7 @@ function App() {
             <img src="/img4.jpg" alt="Collage Image 4" className="collage-image img-pos-4" />
             <img src="/img10.jpg" alt="Collage Image 5" className="collage-image img-pos-5" />
             <img src="/img18.jpg" alt="Collage Image 6" className="collage-image img-pos-6" />
-            <img src="/img24.jpg" alt="Collage Image 7" className="collage-image img-pos-7" /> {/* This one is for Tasmania in the example */}
+            <img src="/img24-1.jpg" alt="Collage Image 7" className="collage-image img-pos-7" /> {/* This one is for Tasmania in the example */}
           </div>
         </div>
       </div>
@@ -70,38 +70,4 @@ export default App;
 
 
 
-/*import './App.css';
 
-function App() {
-  return (
-    <div className="overall-content">
-      <div className="left-section">
-        <div className="left-section-content">
-          <h2 className="heading">22–23 NOVEMBER, 2025 | ICC, SYDNEY</h2>
-          <p className="cont1">
-            Get ready for the <strong>Southern Hemisphere’s largest crypto event</strong>, uniting <strong>Industry leaders, Investors, Innovators, and Crypto Enthusiasts</strong> under one roof.
-          </p>
-          <button className="btn"><span>➜</span> BECOME A SPONSOR</button>
-        </div>
-      </div>
-
-      
-      <div className="right-section">
-        <div className="australia-mask">
-          <div className="image-grid">
-            <img src="/img1.jpg" alt="Grid 1" />
-            <img src="/img2.jpg" alt="Grid 2" />
-            <img src="/img3.jpg" alt="Grid 3" />
-            <img src="/img4.jpg" alt="Grid 4" />
-            <img src="/img5.jpg" alt="Grid 5" />
-            <img src="/img6.jpg" alt="Grid 6" />
-            <img src="/img7.jpg" alt="Grid 7" />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-}
-
-export default App;
-*/
